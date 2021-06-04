4b066baab82a71e28738312cf970499c7f2aa54b
kiethoang214 <48423316+kiethoang214@users.noreply.github.com>
2021-06-04 16:27:06 +0700

    Sum of 2 numbers using data-binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'Sum2Nums';
+@Component({                                
+  selector: 'app-root',                                 //where to displat template
+  templateUrl: './app.component.html',                  //template
+  styleUrls: ['./app.component.css']                    //css for template
+})  
+export class AppComponent {                             
+  title = 'GettingStarted';
+  num1 : string;
+  num2 : string; 
+  result: number;
+  constructor() { 
+    // Initialization inside the constructor
+    this.num1 = "";
+    this.num2 = "";
+    this.result = 0;
+  }
+  AddNumber() {
+    this.result = parseInt(this.num1) + parseInt(this.num2);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
+
+  declarations: [                               //define components
     AppComponent
   ],
-  imports: [
+
+  imports: [                                    //import module
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],                                //service for all component in this module
+  bootstrap: [AppComponent]                     //root module, include root component
 })
 export class AppModule { }
